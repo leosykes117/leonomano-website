68f95d2cb7d4c0956e9789647a6ca62fc51bb017
Leonardo Arellano Martinez <leo.aremtz98@gmail.com>
2023-05-02 23:43:18 -0600

    Se agrega bootstrap y tipografías.
    * Se añaden las dependencias para bootstrap y scss para estilos.
    
    * Se agregan las tipografías de Comfortaa y Chivo.

---
+++ src/components/__tests__/TheWelcome.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import TheWelcome from '../TheWelcome.vue'
+
+describe('TheWelcome', () => {
+  it('renders properly', () => {})
+})
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import assets
+import './assets/scss/main.scss'
+
+// import plugins
+import '@/plugins/bootstrap'
 
 const app = createApp(App)
 
